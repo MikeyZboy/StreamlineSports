be99a3d38638c43ceab2b7e5efea054a08644776
Mike Zahuta <mike.zahuta@gmail.com>
2020-10-20 22:54:25 -0400

    add 4 html pages (sports, leagues, teams, index). CSS in v2 wireframe.  JS functions v1, not displaying.

--- app.js
+++ app.js
@@ -24,19 +24,132 @@
 
 const APIKEY = '1'
 const BASE_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}`
-const SEARCH_TEAM_NAME_URL =`https://www.thesportsdb.com/api/v1/json/${APIKEY}/searchteams.php?t=Arsenal`
-const NEXT_FIVE_TEAM_EVENTS_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/eventsnext.php?id=133602`
+const SEARCH_TEAM_NAME_URL =`https://www.thesportsdb.com/api/v1/json/${APIKEY}/searchteams.php`
+const NEXT_FIVE_TEAM_EVENTS_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/eventsnext.php?id=133602` //this is set to Liverpool already...needs to be dynamic and the filter needs to be passed in via a search value
+const ALL_SPORTS_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/all_sports.php`
+const ALL_LEAGUES_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/all_leagues.php`
+
+
+////// Functions ///////
+
+// getSports = async() => {
+//     try{
+//         const sports = await axios.get(ALL_SPORTS_URL)
+//         //console.log(sports.data)
+//         // console.log(Object.keys(sports.data))
+//         // let allSports = Object.keys(sports.data)
+//         // console.log(allSports) // this isn't right...just returning a single 'sports'
+//         // for (let i = 0; i < allSports.length; i++){
+//         //     let sport = document.createElement('div')
+//         //     sport.innerText = allSports[i]
+//         //     // how do i inject the results into the links in the dropdown?
+
+//         // }
+//     }
+//     catch (error) {
+//         console.log(error)
+//     }
+// }
+//getSports()
+
+
+// getTeams = async() => {
+//     try{
+//         const teams = await axios.get(SEARCH_TEAM_NAME_URL)
+//         // console.log(teams)
+//         // buildDropdown(teams.data)
+//     }
+//     catch (error) {
+//         // console.log(error)
+//     }
+// }
+// getTeams()
+
+
+// let eventData = events.data
+// let formattedEvents = {
+//     match:eventData.strEvent,
+//     matchDate:eventData.dateEvent,
+//     startTime:eventData.strTime,
+//     venue:eventData.strVenue, 
+//     competition:eventData.strLeague  
+// }
+
+// const events = response.data.events
 
 const getEvents = async () => {
-    // confirmed with console.log
+    
     try {
         const response = await axios.get(NEXT_FIVE_TEAM_EVENTS_URL)
-        console.log(response)
-        // buildDropdown(response.data)
-        } catch (error) {
-            // console.log(error)
-        } 
-} 
+        console.log(response.data.events)  // we have logged, trying to return the 5 events for use    
+        // buildDisplay(response.data.events)
+    } catch (error){
+        console.log(error)
+    }
+}
 getEvents()
 
+const getEventsData = async (events) => {
+    const NEXT_FIVE_TEAM_EVENTS_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/eventsnext.php?id=133602`
+    try {
+        const events = await axios.get(NEXT_FIVE_TEAM_EVENTS_URL)
+        //console.log(events.data) // this is returning an array of just 5 events, but why is it already filtered to Liverpool?
+        let eventData = events.data
+        displayEventData(eventData)
+        } catch (error) {
+            console.log(error)
+    } 
+}
+
+const buildDisplay = (events) => {
+    const displayArea = document.querySelector('.events')
+    const dropdown = document.createElement('select')
+    dropdown.addEventListener('change', getEventsData)
+    events.forEach((event) => {
+        let displayDiv = document.createElement('div')
+        displayDiv.innerText = `${event.match} - ${event.matchDate} - ${event.startTime}`
+        let gameDetails = displayDiv.innerText
+        dropdown.appendChild(gameDetails)
+    })
+displayArea.appendChild(dropdown)
+}
+
+// //no errors, but not displaying...
+
+const displayEventData = (eventData) => {
+    let searchArea = document.querySelector('.search')
+    let resultWrapper = document.createElement('div')
+    let resultHeader = document.createElement('h2') // might want to use this for the img src element
+    let currentEventsDisplay = e
+
+    resultWrapper.className = 'search-result'
+    resultHeader.innerText = eventData.match
+
+    resultWrapper.appendChild(resultHeader)
+    resultWrapper.appendChild(currentEventsDisplay)
+    searchArea.appendChild(resultWrapper)
+}
+
+window.onload = getEvents
+
+// can I use OOP to create a grid/image system that is dynamic?
+// use buildDisplay as a class 
+
+
+////// EVENT LISTENERS//////
+
+// this will be an event listener for click that prompts an HTML form about the Calendar sync they'd like...
+// const syncCal = document.querySelector('#calSyncLink')
+// syncCal.addEventListener('click', )
+
+// I need to add eventlisteners for the different sports/leagues/teams menus to pass through the value to the correct API
+
+// const filterTeam = () => {
+// const teamInput = document.getElementById('teams')
+// teamInput.addEventListener('click', function(e) {
+//     e.preventDefault()
+//     let input = document.getElementById('#click').value
+    
+// })
+// }
 
---
+++ scratch.js
@@ -0,0 +1,39 @@
+// Problems to work through
+
+/**
+ * Wednesday 10.21
+ * 1. index.html should be a landing page - description, why it exists, links to nav the site
+ * 2. links to each other html page should be direct from nav bar up top
+ * 3. image in the middle, could be a calendar?
+ * 4. sports, leagues, teams, OR events could be the last link -> send to a calendar page of events for month (need to confirm with API)
+ * 5. data needs to be displayed in each of those html pages referencing respective APIs
+ * 
+ * 
+ * 
+ * Thursday 10.22
+ * 1. figure out the picture grid
+ * 2. should i do the events page too?
+ * 
+ * 
+ * 
+ * 
+ 
+    APIs - need to be have an additional input on end passed through via eventListeners
+ *      search for team by name
+ *      search for event by event name
+ *      list all sports (in dropdown)
+ *      list all leagues (in dropdown)
+ *      "list all users loved teams and players" - filter to teams for the dropdown, could be good way.
+ * 
+ *  teams are identified by idTeam: "133662" six digit strings
+ *  also in that object is strTeam: "Werder Bremen"
+ * 
+ *      livescores 
+ *      live events on today...
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
