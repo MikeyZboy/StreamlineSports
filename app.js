e44da8d0def5451d9540b867a377a4a8218cef78
Mike Zahuta <mike.zahuta@gmail.com>
2020-10-22 20:33:14 -0400

    functioning sports page with click through buttons. Sports->Leagues->Teams->Team styled and working.

--- app.js
+++ app.js
@@ -27,11 +27,11 @@
 
 const APIKEY = '4013017'
 const BASE_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}`
-const SEARCH_TEAM_NAME_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/searchteams.php`
+const TEAM_ID_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/lookupteam.php?id=`
 const NEXT_FIVE_TEAM_EVENTS_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/eventsnext.php?id=133602` //this is set to Liverpool already...needs to be dynamic and the filter needs to be passed in via a search value
 const ALL_SPORTS_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/all_sports.php`
 const ALL_LEAGUES_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/all_leagues.php`
-const ALL_TEAMS_IN_LEAGUE_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/search_all_teams.php?l=English%20Premier%20League`
+const ALL_TEAMS_IN_LEAGUE_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/search_all_teams.php?l=`
 const LIVE_SOCCER_SCORES_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/latestsoccer.php`
 const EVENTS_ON_TV = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/eventstv.php?`
 
@@ -40,10 +40,10 @@ const EVENTS_ON_TV = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/eventstv
 let body = document.querySelector('body')
 let section = document.querySelector('section')
 let soccer = document.querySelector('#soccer')
-let sports = document.querySelector('#sportsSection')
-let leagues = document.querySelector('.leaguesList')
-let teams = document.querySelector('.teams')
+let leagues = document.querySelector('#leaguesList')
+let team = document.querySelector('.team')
 let allSports = document.querySelector('#sportsList')
+
 
 
 
@@ -72,49 +72,100 @@ const liveSoccerScores = () => {
         console.log(error)
     }
 }
-liveSoccerScores()
+// liveSoccerScores()
 
 // this stopped display at 10pm and is now throwing an error "cannot read property length"...looks like the erray is empty now...
 // switch statement / do while loop to correct the "no current games" scenario
 
 
-const allSportsList = () => {
+const allSportsList = async () => {
     try{
-        axios.get(ALL_SPORTS_URL)
-        .then(response => {
+        const response = await axios.get(ALL_SPORTS_URL)
         let sportsList = response.data.sports
+        let sportDiv = document.createElement('div')
+        sportDiv.classList.add('sportDiv')
+        // console.log(sportsList)
         for (let s = 0; s < sportsList.length; s++){
-            let sportTab = document.createElement('li')
-            sportTab.className = 'list'
-            sportTab.innerText = `${sportsList[s].strSport}`
-            allSports.appendChild(sportTab)
+            let sportTab = document.createElement('div')
+            let title = document.createElement('h2')
+            let img = document.createElement('img')
+            let description = document.createElement('p')
+            let leaguesBtn = document.createElement('button')
+            /////// creating elements
+            leaguesBtn.innerHTML = 'View Leagues'
+            leaguesBtn.classList.add('leaguesBtn')
+            sportTab.id = sportsList[s].strSport
+            sportTab.classList.add('sportTab')
+            img.classList.add('sportImage')
+            description.classList.add('summary')
+            title.innerText = sportsList[s].strSport
+            img.src = sportsList[s].strSportThumb
+            description.innerText = sportsList[s].strSportDescription
+            /////// detailing elements
+            sportTab.addEventListener('mouseover', function(){
+                description.style.display = 'block'
+                leaguesBtn.style.display = 'block'
+            })
+            sportTab.addEventListener('mouseout', function(){
+                description.style.display = 'none'
+                leaguesBtn.style.display = 'none'
+            })
+            leaguesBtn.addEventListener('click', function(){
+                allLeaguesList(sportsList[s].strSport)
+            })
+            ////// add Event Listeners
+            sportTab.appendChild(title)
+            sportTab.appendChild(img)
+            sportTab.appendChild(description)
+            sportTab.appendChild(leaguesBtn)
+            sportDiv.appendChild(sportTab)
+            ////// adding elements to document
         }
-        })
+        allSports.appendChild(sportDiv)
     }catch(error){
         console.log(error)
     }
 }
-allSportsList()
 
 
-const allLeaguesList = () => {
+const allLeaguesList = async (sport) => {
+    console.log(sport)
+    allSports.style.display = 'none'
+    document.querySelector('.sportDiv').style.display = 'none'
+    let leagues = document.querySelector('.leagues')
+    leagues.style.display = 'block'
+    let leagueTitle = document.createElement('h1')
+    leagueTitle.innerText = `${sport}`
+    let allLeagues = document.createElement('div')
+    allLeagues.classList.add('allLeagues')
+    leagues.appendChild(leagueTitle)
+    leagues.appendChild(allLeagues)
     try{
-        axios.get(ALL_LEAGUES_URL)
-        .then(response => {
-        let leaguesList = response.data.leagues
-        for (let l = 0; l < leaguesList.length; l++) {
-            let leagueTab = document.createElement('li')
-            leagueTab.className = 'list'
-            leagueTab.innerText = `${leaguesList[l].strLeague}`
-            // leagueTab.innerHTML = (link to )
-            leagues.appendChild(leagueTab)
-        }
+        let response = await axios.get(ALL_LEAGUES_URL)
+        let leaguesList = response.data.leagues.filter(league => league.strSport === sport && league.strLeague.charAt(0) !== '_')
+        console.log(leaguesList)
+        leaguesList.forEach(league => {
+            let leagueDiv = document.createElement('div')
+            leagueDiv.classList.add('leagueDiv')
+            let name = document.createElement('h2')
+            name.classList.add('leagueName')
+            name.innerText = league.strLeague
+            leagueDiv.appendChild(name)
+            leagueDiv.addEventListener('click', function(){
+                teamsInLeague(league.strLeague)
+            })
+            allLeagues.appendChild(leagueDiv)
         })
-        } catch(error) {
-        console.log(error)
-        }
     }
-allLeaguesList()
+    catch(error) {
+        console.log(error)
+    }
+}
+// allLeaguesList()
+
+
+
+
 
 // need to add an event listener for the league that is clicked on => push the string value to the teams_in_league api and take user to teams.html
 // on teams.html = list out all teams in that league
@@ -122,32 +173,75 @@ allLeaguesList()
 // -> display the next 5 events in html form pop up, ask them to fill out for email info of those games/reminder
 
 
-const teamsInLeague = () => {
+const teamsInLeague = async (name) => {
+    document.querySelector('.allLeagues').style.display = 'none'
+    let allTeams = document.createElement('div')
+    allTeams.classList.add('allTeams')
+    let leagueTitle = document.createElement('h1')
+    leagueTitle.innerText = `${name}`
+    team.appendChild(leagueTitle)
     try {
-        axios.get(ALL_TEAMS_IN_LEAGUE_URL)
-        .then(response => {
-            console.log(response.data.teams)
+        let response = await axios.get(ALL_TEAMS_IN_LEAGUE_URL+name)
         let teamsList = response.data.teams
-        for (let t = 0; t < teamsList.length; t++) {
-            // let teamIcon = document.createElement('img')
-            // teamIcon.innerHTML = <img>${teamsList[t].strTeamBadge}</img>
-            let teamTab = document.createElement('li')
-            teamTab.className = 'list'
-            teamTab.innerText = `${teamsList[t].strTeam}`
-            teams.appendChild(teamTab)
-        }
+        console.log(teamsList)
+        teamsList.forEach(team => {
+            let teamDiv = document.createElement('div')
+            let teamTitle = document.createElement('h2')
+            let teamImg = document.createElement('img')
+            teamTitle.innerText = team.strTeam
+            teamImg.src = team.strTeamBadge
+            teamDiv.appendChild(teamTitle)
+            teamDiv.appendChild(teamImg)
+            teamDiv.addEventListener('click', function(){
+                displayTeam(team.idTeam)
+            })
+            allTeams.appendChild(teamDiv)
         })
-    }catch(error){
+    }
+    catch(error){
+        console.log(error)
+    }
+    team.appendChild(allTeams)
+}
+// teamsInLeague()
+
+
+const displayTeam = async (id) => {
+    console.log(id)
+    document.querySelector('.allTeams').style.display = 'none'
+    try{
+        let response = await axios.get(TEAM_ID_URL+id)
+        console.log(response.data.teams)
+        let teams = response.data.teams[0]
+        let teamTitle = document.createElement('h1')
+        let teamBanner = document.createElement('img')
+        teamBanner.classList.add('teamBanner')
+        teamTitle.innerText = teams.strTeam
+        teamBanner.src = teams.strTeamBanner
+        team.appendChild(teamBanner)
+    }
+    catch(error){
         console.log(error)
     }
 }
-teamsInLeague()
 
+// example for trying to make a dynamic 'a' tag
+// var img11=document.createElement("a");
+
+// img11.href='http://google.de';
+// img11.innerHTML = 'Google';
+
+// document.body.appendChild( img11 );
 
 
 // TO DO THURSDAY //
 
 /// EVENT LISTENERS / FILTERS ///
+
+
+
+
+
 
 // dropdown options from Sports/Leagues/Teams
 
@@ -159,27 +253,11 @@ teamsInLeague()
 
 
 
-
-
-// html form input from text on aside -> date, team you're looking for?
-
-// calendar onload -> today's date to display today's events?
-// could do a form - today is the ""
-// what day are you looking for an event?
-
-
-
-
-
-
-
-
-
 // for homepage...
 // this will need a current date injected into the API URL to filter the results correctly..can I do that via a calendar or an HTML form?
 // input a calendar? accomplish 2 things: 1. take today's date and push into function/api url below. 2. allow user to choose a date and see what's on tv.
 
-// const televisedEvents = () => {
+// const eventsToday = () => {
 //     //console.log('this sucks')
 //     try{
 //         axios.get(EVENTS_ON_TV)
@@ -190,4 +268,7 @@ teamsInLeague()
 //         console.log(error)
 //     }
 // }
-// televisedEvents()
+// eventsToday()
+
+
+document.addEventListener('DOMContentLoaded', allSportsList)
--- scratch.js
+++ scratch.js
@@ -1,6 +1,147 @@
 // Problems to work through
 // DONT THINK ABOUT USING OOP ON THIS - MORE TROUBLE THAN WORTH
 /**
+ * 
+ * most current version 11:18-thurs
+ * 
+ * 
+ * const APIKEY = '4013017'
+const BASE_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}`
+const SEARCH_TEAM_NAME_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/searchteams.php`
+const NEXT_FIVE_TEAM_EVENTS_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/eventsnext.php?id=133602` //this is set to Liverpool already...needs to be dynamic and the filter needs to be passed in via a search value
+const ALL_SPORTS_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/all_sports.php`
+const ALL_LEAGUES_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/all_leagues.php`
+const ALL_TEAMS_IN_LEAGUE_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/search_all_teams.php?l=English%20Premier%20League`
+const LIVE_SOCCER_SCORES_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/latestsoccer.php`
+const EVENTS_ON_TV = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/eventstv.php?`
+
+
+
+let body = document.querySelector('body')
+let section = document.querySelector('section')
+let soccer = document.querySelector('#soccer')
+let sports = document.querySelector('#sportsSection')
+let leagues = document.querySelector('#leaguesList')
+let teams = document.querySelector('.teams')
+let allSports = document.querySelector('#sportsList')
+
+
+
+
+
+
+////// Functions ///////
+
+
+const liveSoccerScores = () => {
+    try {
+        axios.get(LIVE_SOCCER_SCORES_URL)
+            //console.log(response) // returns array of live soccer scores
+            .then(response => {
+                //console.log(response.data.teams.Match[0])
+                // console.log(response.data.teams.Match[0].HomeTeam)
+                let matchArr = response.data.teams.Match
+                for (let i = 0; i < matchArr.length; i++) {
+                    //console.log('hey there')
+                    let matchScore = document.createElement('option')
+                    matchScore.innerText = `Minute: ${matchArr[i].Time} ~ ${matchArr[i].AwayTeam}: ${matchArr[i].AwayGoals} - ${matchArr[i].HomeTeam}: ${matchArr[i].HomeGoals}`
+                    soccer.appendChild(matchScore)
+                }
+            })
+    } catch (error) {
+        console.log(error)
+    }
+}
+liveSoccerScores()
+
+// this stopped display at 10pm and is now throwing an error "cannot read property length"...looks like the erray is empty now...
+// switch statement / do while loop to correct the "no current games" scenario
+
+
+const allSportsList = () => {
+    try{
+        axios.get(ALL_SPORTS_URL)
+        .then(response => {
+        let sportsList = response.data.sports
+        for (let s = 0; s < sportsList.length; s++){
+            let sportTab = document.createElement('li')
+            sportTab.className = 'list'
+            sportTab.innerText = `${sportsList[s].strSport}`
+            allSports.appendChild(sportTab)
+        }
+        })
+    }catch(error){
+        console.log(error)
+    }
+}
+allSportsList()
+
+
+const allLeaguesList = () => {
+    try{
+        axios.get(ALL_LEAGUES_URL)
+        .then(response => {
+        let leaguesList = response.data.leagues
+        for (let l = 0; l < leaguesList.length; l++) {
+            let leagueTab = document.createElement('option')
+            leagueTab.className = 'list'
+            leagueTab.innerText = `${leaguesList[l].strLeague}`
+            // leagueTab.innerHTML = (link to )
+            leagues.appendChild(leagueTab)
+        }
+        })
+        } catch(error) {
+        console.log(error)
+        }
+    }
+allLeaguesList()
+
+// need to add an event listener for the league that is clicked on => push the string value to the teams_in_league api and take user to teams.html
+// on teams.html = list out all teams in that league
+// click on team (event listener passes in team ID to get next 5 events api
+// -> display the next 5 events in html form pop up, ask them to fill out for email info of those games/reminder
+
+
+const teamsInLeague = () => {
+    try {
+        axios.get(ALL_TEAMS_IN_LEAGUE_URL)
+        .then(response => {
+            //console.log(response.data.teams)
+        let teamsList = response.data.teams
+        for (let t = 0; t < teamsList.length; t++) {
+            console.log(teamsList)
+            let teamTab = document.createElement('a')
+            teamTab.className = 'list'
+            teamTab.href = `${NEXT_FIVE_TEAM_EVENTS_URL}`
+            teamTab.innerHTML = `${teamsList[t].strTeam}`
+            teams.appendChild(teamTab)
+        }
+        })
+    }catch(error){
+        console.log(error)
+    }
+}
+teamsInLeague()
+
+const buildLeaguesDropdown = () => {
+    const dropdownDiv = document.querySelector('.dropdown')
+    const dropdown = document.createElement('select')  //<-- select elements are dropdowns
+    dropdown.addEventListener('change', allLeaguesList) //<-- change is looking for any change in that element
+    // data.forEach((symbol) => {
+    //     let optionElement = document.createElement('option')
+    //     optionElement.innerText = `${symbol.description} - ${symbol.symbol}`
+    //     optionElement.setAttribute('value', symbol.symbol)
+    //     dropdown.appendChild(optionElement)
+    dropdownDiv.appendChild(dropdown)
+}
+buildLeaguesDropdown()
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  * Wednesday 10.21
  * 1. index.html should be a landing page - description, why it exists, links to nav the site
  * 2. links to each other html page should be direct from nav bar up top
@@ -277,152 +418,153 @@ const leaguesOnly = () => {
 // }
 
 
-* {
-    box-sizing: border-box;
-}
+// * {
+//     box-sizing: border-box;
+// }
 
-body {
-    display: grid;
-    grid-template-columns: 1fr 4fr;
-    grid-template-rows: 75px 1fr 30px;
-    text-align: center;
-    height: 100vh;
-    margin: 0;
-    font-family: 'Commissioner', sans-serif;
-    background-color: rgb(255, 255, 255);
-}
+// body {
+//     display: grid;
+//     grid-template-columns: 1fr 4fr;
+//     grid-template-rows: 75px 1fr 30px;
+//     text-align: center;
+//     height: 100vh;
+//     margin: 0;
+//     font-family: 'Commissioner', sans-serif;
+//     background-color: rgb(255, 255, 255);
+// }
 
-.flex-ctr {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #00B2A9;
-}
+// .flex-ctr {
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     background-color: #00B2A9;
+// }
 
-h1 {
-    display: flex;
-    margin: 0 auto;
-    padding: 23px;
-    background-color: white;
-}
+// h1 {
+//     display: flex;
+//     margin: 0 auto;
+//     padding: 23px;
+//     background-color: white;
+// }
 
-#homepage {
-    grid-column: 1;
-    font-weight: 600;
-    font-size: xx-large;
-    color: #00B2A9;
-}
+// #homepage {
+//     grid-column: 1;
+//     font-weight: 600;
+//     font-size: xx-large;
+//     color: #00B2A9;
+// }
 
-span {
-    justify-content: space-evenly;
-    grid-column-start: 2;
-    color: white;
-    font-family: 'Commissioner', sans-serif;
-}
+// span {
+//     justify-content: space-evenly;
+//     grid-column-start: 2;
+//     color: white;
+//     font-family: 'Commissioner', sans-serif;
+// }
 
-header {
-    display: inline-grid;
-    grid-column-start: 2;
-    background-color: #00B2A9;
-    border: 5px solid white;
-}
+// header {
+//     display: inline-grid;
+//     grid-column-start: 2;
+//     background-color: #00B2A9;
+//     border: 5px solid white;
+// }
 
-.logo {
-    position: relative;
-    top: 0;
-    left: 0;
-    background: white;
-}
+// .logo {
+//     position: relative;
+//     top: 0;
+//     left: 0;
+//     background: white;
+// }
 
-div {
-    display: inline-block;
-    font-size: 25px;
-    margin: 10px;
-    color: #FFFFFF;
-    padding: 10px;
-    grid-row-end: span 2;
-}
+// div {
+//     display: inline-block;
+//     font-size: 25px;
+//     margin: 10px;
+//     color: #FFFFFF;
+//     padding: 10px;
+//     grid-row-end: span 2;
+// }
 
-.events {
-    background-color: white;
-    height: 75%;
-    width: 75%;
-    position: absolute;
-    right: 0;
-}
+// .events {
+//     background-color: white;
+//     height: 75%;
+//     width: 75%;
+//     position: absolute;
+//     right: 0;
+// }
 
-.grid {
-    display: grid;
-    grid-template-columns: repeat(8, 1fr);
-    grid-template-rows: repeat(8, 5vw);
-    grid-gap: 15px;
-}
+// .grid {
+//     display: grid;
+//     grid-template-columns: repeat(8, 1fr);
+//     grid-template-rows: repeat(8, 5vw);
+//     grid-gap: 15px;
+// }
 
-footer {
-    grid-column-end: span 2;
-}
+// footer {
+//     grid-column-end: span 2;
+// }
 
-/* #allSportsList{
-    display: grid;
-    max-width: 75%; 
-}*/
+// /* #allSportsList{
+//     display: grid;
+//     max-width: 75%; 
+// }*/
 
-a {
-    color: white;
-    text-decoration: none;
-    order: 5;
-}
+// a {
+//     color: white;
+//     text-decoration: none;
+//     order: 5;
+// }
 
-.links {
-    position: relative;
-    display: inline-block;
-    padding: 1em;
-    display: flex;
-    justify-content: center;
-}
+// .links {
+//     position: relative;
+//     display: inline-block;
+//     padding: 1em;
+//     display: flex;
+//     justify-content: center;
+// }
 
-select {
-    padding:1em;
-}
+// select {
+//     padding:1em;
+// }
 
-select option {
-    padding:1em;
-}
+// select option {
+//     padding:1em;
+// }
 
-.search {
-    display: flex;
-    flex-wrap: wrap;
-}
+// .search {
+//     display: flex;
+//     flex-wrap: wrap;
+// }
 
-.search .search-result {
-    background-color: #fafafa;
-    text-align: center;
-    border:1px solid #bdbdbd;
-    border-radius: 4px;
-    width: 60%;
-    margin: 1em auto;
-}
+// .search .search-result {
+//     background-color: #fafafa;
+//     text-align: center;
+//     border:1px solid #bdbdbd;
+//     border-radius: 4px;
+//     width: 60%;
+//     margin: 1em auto;
+// }
 
-.teamsList {
-    list-style-type: none;
-}
+// .teamsList {
+//     list-style-type: none;
+// }
 
-/* trying to style the dropdown nav */
+// /* trying to style the dropdown nav */
 
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
+// .dropdown-content {
+//   display: none;
+//   position: absolute;
+//   background-color: #f9f9f9;
+//   min-width: 160px;
+//   box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+//   z-index: 1;
+// }
 
-.dropdown-content a {
-    float: none;
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-    text-align: left;
-  }
+// .dropdown-content a {
+//     float: none;
+//     color: black;
+//     padding: 12px 16px;
+//     text-decoration: none;
+//     display: block;
+//     text-align: left;
+//   }
+
