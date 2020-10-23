572f76f822979c4ea1462028aa8cd149b555d648
Mike Zahuta <mike.zahuta@gmail.com>
2020-10-23 01:21:00 -0400

    finish homepage, pic and button working

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
-/**
- * StreamlineSports 
- * - customize sports to your life, not the other way around
- * Useful API links from TheSportsDb
- * 
- * DOMAIN: `https://www.thesportsdb.com/`
- * APIKEY: '1'
- * BASE_URL: `https://www.thesportsdb.com/api/v1/json/${APIKEY}`
- * Next 5 Events by Team Id: 'https://www.thesportsdb.com/api/v1/json/1/eventsnext.php?id=133602'
- * 
- * backup plan: "http://www.espn.com/apis/devcenter/io-docs.html?ref=public-apis"
- * 
- * 1. define global variables
- * 2. test a function that grabs data from the API
- * 3. what type are we getting? [keys, values, etc.]
- * 
- */
-
-
 //  console.log('This is Working!') <-- confirmed
 
-
-
+////**** Streamline Sports ****////
 
 //// Declaring Variables ////
-
 
 const APIKEY = '4013017'
 const BASE_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}`
@@ -35,7 +14,6 @@ const ALL_TEAMS_IN_LEAGUE_URL = `https://www.thesportsdb.com/api/v1/json/${APIKE
 const LIVE_SOCCER_SCORES_URL = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/latestsoccer.php`
 const EVENTS_ON_TV = `https://www.thesportsdb.com/api/v1/json/${APIKEY}/eventstv.php?`
 
-
 let body = document.querySelector('body')
 let section = document.querySelector('section')
 let soccer = document.querySelector('#soccer')
@@ -43,25 +21,7 @@ let leagues = document.querySelector('#leaguesList')
 let team = document.querySelector('.team')
 let allSports = document.querySelector('#sportsList')
 
-
 ////// Functions ///////
-
-
-const liveSoccerScores = () => {
-    try {
-        axios.get(LIVE_SOCCER_SCORES_URL)
-            .then(response => {
-                let matchArr = response.data.teams.Match
-                for (let i = 0; i < matchArr.length; i++) {
-                    let matchScore = document.createElement('option')
-                    matchScore.innerText = `Minute: ${matchArr[i].Time} ~ ${matchArr[i].AwayTeam}: ${matchArr[i].AwayGoals} - ${matchArr[i].HomeTeam}: ${matchArr[i].HomeGoals}`
-                    soccer.appendChild(matchScore)
-                }
-            })
-    } catch (error) {
-        console.log(error)
-    }
-}
 
 const allSportsList = async () => {
     try{
@@ -69,7 +29,6 @@ const allSportsList = async () => {
         let sportsList = response.data.sports
         let sportDiv = document.createElement('div')
         sportDiv.classList.add('sportDiv')
-        // console.log(sportsList)
         for (let s = 0; s < sportsList.length; s++){
             let sportTab = document.createElement('div')
             let title = document.createElement('h2')
@@ -112,15 +71,14 @@ const allSportsList = async () => {
     }
 }
 
-
 const allLeaguesList = async (sport) => {
-    console.log(sport)
     allSports.style.display = 'none'
     document.querySelector('.sportDiv').style.display = 'none'
     let leagues = document.querySelector('.leagues')
     leagues.style.display = 'block'
     let leagueTitle = document.createElement('h1')
     leagueTitle.innerText = `${sport}`
+    leagueTitle.style.color = `darkslategray`
     let allLeagues = document.createElement('div')
     allLeagues.classList.add('allLeagues')
     leagues.appendChild(leagueTitle)
@@ -128,7 +86,6 @@ const allLeaguesList = async (sport) => {
     try{
         let response = await axios.get(ALL_LEAGUES_URL)
         let leaguesList = response.data.leagues.filter(league => league.strSport === sport && league.strLeague.charAt(0) !== '_')
-        console.log(leaguesList)
         leaguesList.forEach(league => {
             let leagueDiv = document.createElement('div')
             leagueDiv.classList.add('leagueDiv')
@@ -147,17 +104,13 @@ const allLeaguesList = async (sport) => {
     }
 }
 
-// need to add an event listener for the league that is clicked on => push the string value to the teams_in_league api and take user to teams.html
-// on teams.html = list out all teams in that league
-// click on team (event listener passes in team ID to get next 5 events api
-// -> display the next 5 events in html form pop up, ask them to fill out for email info of those games/reminder
-
 const teamsInLeague = async (name) => {
     document.querySelector('.allLeagues').style.display = 'none'
     let allTeams = document.createElement('div')
     allTeams.classList.add('allTeams')
     let leagueTitle = document.createElement('h1')
     leagueTitle.innerText = `${name}`
+    leagueTitle.style.color = 'darkslategray'
     team.appendChild(leagueTitle)
     try {
         let response = await axios.get(ALL_TEAMS_IN_LEAGUE_URL+name)
@@ -183,7 +136,6 @@ const teamsInLeague = async (name) => {
     team.appendChild(allTeams)
 }
 
-
 const displayTeam = async (id) => {
     console.log(id)
     document.querySelector('.allTeams').style.display = 'none'
@@ -196,7 +148,7 @@ const displayTeam = async (id) => {
         let teamDescription = document.createElement('div')
         let teamWebsite = document.createElement('div')
         teamWebsite.classList.add('teamWeb')
-        teamWebsite.innerHTML = teams.strWebsite
+        teamWebsite.innerText = teams.strWebsite
         teamDescription.classList.add('teamDetails')
         teamDescription.innerText = teams.strDescriptionEN
         teamBanner.classList.add('teamBanner')
@@ -211,7 +163,39 @@ const displayTeam = async (id) => {
     }
 }
 
-// go back function
+//// Event Listeners ////
+
+document.addEventListener('DOMContentLoaded', allSportsList)
+
+
+// go back functions
+
+// const backToLeagues = () => {
+//     let backButtonDiv = document.createElement('div')
+//     let backButtonLeagues = document.createElement('button')
+//     backButtonLeagues.classList.add('backButton')
+//     backButtonLeagues.innerText = 'Back to Leagues'
+//     backButtonDiv.appendChild(backButtonLeagues)
+//     backButtonLeagues.addEventListener('click', function(){
+//         allLeaguesList()
+//     })
+// }
+
+// const backToTeams = () => {
+//     let backTeamsDiv = document.createElement('div')
+//     let backButtonTeams = document.createElement('button')
+//     backButtonTeams.classList.add('backButton')
+//     backButtonTeams.innerText = 'Back to Teams'
+//     backTeamsDiv.appendChild(backButtonTeams)
+//     team.appendChild(backTeamsDiv)
+//     backButtonTeams.addEventListener('click', function(){
+//         teamsInLeague(name)
+//     })
+// }
+
+
+
+
 
 
 
@@ -230,5 +214,18 @@ const displayTeam = async (id) => {
 // }
 // eventsToday()
 
-
-document.addEventListener('DOMContentLoaded', allSportsList)
+// const liveSoccerScores = () => {
+//     try {
+//         axios.get(LIVE_SOCCER_SCORES_URL)
+//             .then(response => {
+//                 let matchArr = response.data.teams.Match
+//                 for (let i = 0; i < matchArr.length; i++) {
+//                     let matchScore = document.createElement('option')
+//                     matchScore.innerText = `Minute: ${matchArr[i].Time} ~ ${matchArr[i].AwayTeam}: ${matchArr[i].AwayGoals} - ${matchArr[i].HomeTeam}: ${matchArr[i].HomeGoals}`
+//                     soccer.appendChild(matchScore)
+//                 }
+//             })
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
